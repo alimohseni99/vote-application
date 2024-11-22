61cb3185aa951657f2e157f218198a78039bed96
Ali <ali.mohseni05@yahoo.se>
2024-11-22 21:41:15 +0100

    added schema for representatives

---
+++ src/representative-management/schema/schema.ts
@@ -0,0 +1,10 @@
+import { sql } from "drizzle-orm";
+import { pgTable, uuid, varchar } from "drizzle-orm/pg-core";
+
+export const representativesTable = pgTable("representative", {
+  id: uuid()
+    .primaryKey()
+    .default(sql`gen_random_uuid()`),
+  name: varchar({ length: 255 }).notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+});
