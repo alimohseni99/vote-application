8ccdc2649fe63342655a9acb17f51b683ffbf781
Ali <ali.mohseni05@yahoo.se>
2024-12-01 22:56:39 +0100

    fixed spellings in representative repository

--- src/features/representative/repository.ts
+++ src/features/representative/repository.ts
@@ -1,48 +1,25 @@
 import { Db } from "@/db";
-import { and, eq, sql } from "drizzle-orm";
-import { representativesTable, votersTable } from "./schema/schema";
+import { eq, sql } from "drizzle-orm";
+import { representativeTable } from "./schema/schema";
 import { Representative } from "./type";
 
 export function createRepository(db: Db) {
   return {
     async getAllRepresentatives() {
-      return await db.select().from(representativesTable);
+      return await db.select().from(representativeTable);
     },
     async addRepresentative(representative: Representative) {
       return await db
-        .insert(representativesTable)
+        .insert(representativeTable)
         .values(representative)
         .execute();
     },
 
-    async addPublicVote(id: string, representativeId: string) {
-      const publicVote = await db
-        .insert(votersTable)
-        .values({ id, representativeId });
-      if (!publicVote) {
-        throw new Error("Something went wrong");
-      }
-
+    async addPublicVote(representativeId: string) {
       await db
-        .update(representativesTable)
-        .set({ votes: sql`${representativesTable.votes} +1` })
-        .where(eq(representativesTable.id, representativeId));
-    },
-    async checkForDuplicateVote(
-      publicVoterId: string,
-      representativeId: string
-    ): Promise<boolean> {
-      const result = await db
-        .select()
-        .from(votersTable)
-        .where(
-          and(
-            eq(votersTable.id, publicVoterId),
-            eq(votersTable.representativeId, representativeId)
-          )
-        );
-
-      return result.length > 0;
+        .update(representativeTable)
+        .set({ publicVotes: sql`${representativeTable.publicVotes} +1` })
+        .where(eq(representativeTable.id, representativeId));
     },
   };
 }
