97becadb87fa6d2b70fc0f5a466c85d6b886e889
Ali <ali.mohseni05@yahoo.se>
2024-12-01 21:44:49 +0100

    updated representative schema

--- src/features/representative/schema/schema.ts
+++ src/features/representative/schema/schema.ts
@@ -7,32 +7,6 @@ export const representativesTable = pgTable("representative", {
     .default(sql`gen_random_uuid()`),
   name: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
-  votes: integer().notNull().default(0),
+  publicVotes: integer().notNull().default(0),
 });
 
-export const votersTable = pgTable("public_voter", {
-  id: uuid()
-    .primaryKey()
-    .default(sql`gen_random_uuid()`),
-  representativeId: uuid().references(() => representativesTable.id),
-});
-
-export const representativeVotesTable = pgTable("representative_votesTable", {
-  id: uuid()
-    .primaryKey()
-    .default(sql`gen_random_uuid()`),
-  representativeId: uuid()
-    .notNull()
-    .references(() => representativesTable.id),
-  choice: varchar().notNull(),
-});
-
-export const publicVotesTable = pgTable("public_votesTable", {
-  id: uuid()
-    .primaryKey()
-    .default(sql`gen_random_uuid()`),
-  publicVotersId: uuid()
-    .notNull()
-    .references(() => votersTable.id),
-  choice: varchar().notNull(),
-});
