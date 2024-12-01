db4b51715d9b50cf4096bf745a691bdb70205291
Ali <ali.mohseni05@yahoo.se>
2024-12-01 21:20:21 +0100

    updated election schema

--- src/features/elections/schema/schema.ts
+++ src/features/elections/schema/schema.ts
@@ -1,19 +1,12 @@
 import { sql } from "drizzle-orm";
-import {
-  boolean,
-  pgTable,
-  timestamp,
-  uuid,
-  varchar,
-} from "drizzle-orm/pg-core";
+import { pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
 
 export const electionTable = pgTable("election", {
   id: uuid()
     .primaryKey()
     .default(sql`gen_random_uuid()`),
   title: varchar({ length: 50 }).notNull(),
-  active: boolean().notNull().default(true),
   createdTimeStamp: timestamp().notNull().defaultNow(),
-  deactivatedTimeStamp: timestamp(),
   optionText: varchar({ length: 255 }).array().notNull(),
+  status: varchar({ length: 50 }).notNull(), // can only be 'ongoing' or 'concluded'
 });
