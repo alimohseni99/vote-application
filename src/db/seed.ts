90910fe43996f3dc4306ee6683e5f639a3e8e1fa
Ali <ali.mohseni05@yahoo.se>
2024-12-03 23:21:46 +0100

    added seed for public voter

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -2,6 +2,7 @@ import { electionTable } from "@/features/elections/schema/schema";
 import { faker } from "@faker-js/faker";
 import { db } from ".";
 import { representativeTable } from "../features/representative/schema/schema";
+import { publicVotersTable } from "./../features/public/schema/schema";
 const representativeSeed = async () => {
   const representative: (typeof representativeTable.$inferInsert)[] = [];
 
@@ -9,7 +10,7 @@ const representativeSeed = async () => {
     representative.push({
       name: faker.person.fullName(),
       email: faker.internet.email(),
-      publicVotes: faker.number.int({ min: 10, max: 100 }),
+      publicVotes: faker.number.int({ min: 1, max: 1000 }),
     });
   }
   representative.push({
@@ -37,3 +38,20 @@ const electionSeed = async () => {
 };
 
 electionSeed().then(() => console.log("seeded election table"));
+
+const publicVotersSeed = async () => {
+  const publicVoter: (typeof publicVotersTable.$inferInsert)[] = [];
+
+  for (let i = 0; i < 10; i++) {
+    publicVoter.push({
+      id: faker.string.uuid(),
+    });
+  }
+  publicVoter.push({
+    id: "c7a1ed89-68db-4c4f-8e5b-d3182bfa5c5d",
+  });
+
+  await db.insert(publicVotersTable).values(publicVoter).execute();
+};
+
+publicVotersSeed().then(() => console.log("seeded public-voter table"));
--- src/features/public/schema/schema.ts
+++ src/features/public/schema/schema.ts
@@ -1,5 +1,11 @@
+import { sql } from "drizzle-orm";
 import { pgTable, uuid } from "drizzle-orm/pg-core";
 
 export const publicVotersTable = pgTable("public_voters", {
-  id: uuid().primaryKey().default("c7a1ed89-68db-4c4f-8e5b-d3182bfa5c5d"),
+  id: uuid()
+    .primaryKey()
+    .default(sql`gen_random_uuid()`),
 });
+
+export const publicVoterInsert = typeof publicVotersTable.$inferInsert;
+export const publicVoterSelect = typeof publicVotersTable.$inferSelect;
